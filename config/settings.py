876785673710b592e43149b051ea8e326013ee2a
megarazor <vannampham1301@gmail.com>
2020-02-10 19:54:37 +0700

    WhiteNoise config

--- config/settings.py
+++ config/settings.py
@@ -131,4 +131,4 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
