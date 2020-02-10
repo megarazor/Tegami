2e8bc1689550b302f204b6925bc089863b77a5c6
megarazor <vannampham1301@gmail.com>
2020-02-10 22:50:55 +0700

    Fix STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
